b985f07cd1723d9591183efc6257f1730a9ddabe
Pepo <pepo.ospina@gmail.com>
2018-05-13 17:27:34 +0200

    merge 326b428d

--- src/main/java/org/collectiveone/common/StartupMigrations.java
+++ src/main/java/org/collectiveone/common/StartupMigrations.java
@@ -3,8 +3,13 @@ package org.collectiveone.common;
 
 import javax.transaction.Transactional;
 
+import org.collectiveone.modules.activity.ActivityService;
+import org.collectiveone.modules.activity.repositories.ActivityRepositoryIf;
+import org.collectiveone.modules.activity.repositories.SubscriberRepositoryIf;
+import org.collectiveone.modules.initiatives.repositories.InitiativeRepositoryIf;
+import org.collectiveone.modules.model.repositories.ModelCardWrapperRepositoryIf;
+import org.collectiveone.modules.model.repositories.ModelSectionRepositoryIf;
 import org.collectiveone.modules.users.AppUserRepositoryIf;
-import org.collectiveone.modules.users.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -14,28 +19,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupMigrations implements ApplicationListener<ContextRefreshedEvent> {
 	
+	@Autowired
+	ActivityService activityService;
+		
+	@Autowired
+	ModelCardWrapperRepositoryIf modelCardWrapperRepository;
 	
 	@Autowired
-	AppUserService appUserService	;
+	ActivityRepositoryIf activityRepository;
+	
+	@Autowired
+	ModelSectionRepositoryIf modelSectionRepository;
+	
+	@Autowired
+	InitiativeRepositoryIf initiativeRepository;
 	
 	@Autowired
 	AppUserRepositoryIf appUserRepository;
+	
+	@Autowired
+	SubscriberRepositoryIf subscriberRepository;
 	
 	
 	@EventListener
 	@Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
-		
-		/* Issue with Auth0 API API rate limit */
-//		List<AppUser> users = (List<AppUser>) appUserRepository.findAll();
-//		
-//		System.out.println("updating users profiles");
-//		
-//		for (AppUser user : users) {
-//			appUserService.updateUserDataInLocalDB(user.getC1Id());
-//		}
     }
-	
-	
-
 }
