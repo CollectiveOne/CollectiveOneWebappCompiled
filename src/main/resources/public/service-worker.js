9abba9f30673ddb62f4a4b8231640d59103a25fd
Pepo <pepo.ospina@gmail.com>
2018-11-30 13:17:37 +0100

    merge eb0d3aaab5f02b5447706b5cfd5844a0039687e1

---
+++ src/main/resources/public/service-worker.js
@@ -0,0 +1,21 @@
+importScripts("/precache-manifest.2a7ad792900b5cc9fffbd12443bc6c3a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+if (workbox) {
+    console.log(`Workbox is loaded`);
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+}
+else {
+    console.log(`Workbox didn't load`);
+}
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  console.log(event.notification);
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
+
