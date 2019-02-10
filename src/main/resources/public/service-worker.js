724607542fb9dcc5de533bc31ce6089c944b6c73
Pepo <pepo.ospina@gmail.com>
2019-02-10 18:21:39 +0100

    merge fd9fc5d0eec51806d440ceb2d654aa5808bbd6f2

--- src/main/resources/public/service-worker.js
+++ src/main/resources/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3979eeb6f794f4b53e449366c1129446.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.705e8e46c83eeb51726aaaacd6b96fa3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if (workbox) {
     console.log(`Workbox is loaded`);
